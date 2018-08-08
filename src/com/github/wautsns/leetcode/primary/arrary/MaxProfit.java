6ff7612297a6206c8766ca1d31f36f6d04784d15
wautsns <wautsns@foxmail.com>
2018-08-09 00:02:08 +0800

    初始导入

---
+++ src/com/github/wautsns/leetcode/primary/arrary/MaxProfit.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 wautsns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.wautsns.leetcode.primary.arrary;
+
+/**
+ * <pre>
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ * 题目详细见: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/22/
+ * </pre>
+ *
+ * @author wautsns →http://www.github.com/wautsns←
+ *
+ * @created 2018年8月8日
+ */
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0)
+            return 0;
+        int profit = 0;
+        // 买入的索引位置
+        int buyIndex = 0;
+        for (int i = 1, l = prices.length; i < l; i++)
+            if (prices[i - 1] > prices[i]) { // 判断今日是否比昨日价格低
+                // 今日比昨日价格低,则应该在昨日抛售,并尝试在今日买入
+                profit += prices[i - 1] - prices[buyIndex];
+                buyIndex = i;
+            } // 今日比昨日价格高,则继续观望
+        // 最后一次尝试
+        int lastTry = prices[prices.length - 1] - prices[buyIndex];
+        return lastTry <= 0 ? profit : profit + lastTry;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[] {
+                7, 6, 4, 3, 1
+        };
+        System.out.println(maxProfit(prices));
+    }
+
+    public int standard(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++)
+            if (i + 1 < prices.length && prices[i + 1] - prices[i] > 0)
+                maxProfit += prices[i + 1] - prices[i];
+        return maxProfit;
+    }
+}
---
+++ src/com/github/wautsns/leetcode/primary/arrary/RemoveDuplicates.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 wautsns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.wautsns.leetcode.primary.arrary;
+
+/**
+ * <pre>
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 题目详细见: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/21/
+ * <pre>
+ *
+ * @author wautsns →http://www.github.com/wautsns←
+ *
+ * @created 2018年8月8日
+ */
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        // 新数组长度,同时也标识着下一次放置字符的索引
+        int len = 1;
+        for (int i = 1, l = nums.length, front = nums[0]; i < l; i++)
+            if (front != nums[i]) { // 判断前一个字符与当前字符是否相同
+                // 前一个字符与当前字符相同,则将当前字符置入特定的索引
+                nums[len++] = nums[i];
+                // 前一个字符变为当前字符
+                front = nums[i];
+            }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {
+                0, 0, 1, 1, 1, 2, 2, 3, 3, 4
+        };
+        // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+        int len = removeDuplicates(nums);
+
+        // 在函数里修改输入数组对于调用者是可见的。
+        // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+        for (int i = 0; i < len; i++)
+            System.out.println(nums[i]);
+    }
+
+    public int standard(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i - 1] != nums[i])
+                nums[count++] = nums[i];
+        return count;
+    }
+}
