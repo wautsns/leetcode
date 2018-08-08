2ae67c60d0ccda991093271d0fa9739be720f745
wautsns <wautsns@foxmail.com>
2018-08-09 00:04:01 +0800

    修改包名错误

--- src/com/github/wautsns/leetcode/primary/array/RemoveDuplicates.java
+++ src/com/github/wautsns/leetcode/primary/array/RemoveDuplicates.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.wautsns.leetcode.primary.arrary;
+package com.github.wautsns.leetcode.primary.array;
 
 /**
  * <pre>
